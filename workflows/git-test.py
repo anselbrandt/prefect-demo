9a18aa5b29786569899c55f961627f8c3f9d2575
anselbrandt <ansel@anselbrandt.com>
2025-05-11 21:15:24 -0400

    Update repo_path

--- workflows/git-test.py
+++ workflows/git-test.py
@@ -9,7 +9,7 @@ token = os.getenv("GITHUB_TOKEN")
 if not token:
     raise ValueError("GITHUB_TOKEN not found in .env")
 
-repo_path = Path.cwd()
+repo_path = Path(__file__).resolve().parent.parent
 
 repo = Repo(repo_path)
 assert not repo.bare
